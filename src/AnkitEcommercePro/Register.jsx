44fd0590af3a73026a03c48e905eed88dae5cfb2
Praveen Kannaujiya <praveenkannaujiya0@gmail.com>
2023-05-10 23:06:37 +0530

    ye dusari bar h commit

--- src/AnkitEcommercePro/Register.jsx
+++ src/AnkitEcommercePro/Register.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+  const [name,setName]=useState('')
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+
+  const submitbtn =()=>{
+    let items ={name ,email,password}
+    console.log(items)
+  }
   return (
-    <div>Register</div>
+    <div className='col-sm-4 offset-sm-4'>
+      <h1>User Sign Up</h1>
+      <input type="text" className='form-control' value={name} onChange={(e)=>setName(e.target.value)} /> <br />
+      <input type="text" className='form-control' value={email} onChange={(e)=>setEmail(e.target.value)} /> <br />
+      <input type="text" className='form-control' value={password} onChange={(e)=>setPassword(e.target.value)} /> <br />
+
+      <button className='btn btn-primary' onClick={()=>submitbtn()}>Sign Up</button>
+    </div>
+
   )
 }
 
