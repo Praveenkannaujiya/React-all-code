4a8b7007368ca0837f33d7254d8a5526870c06f8
Praveen Kannaujiya <praveenkannaujiya0@gmail.com>
2023-05-11 06:40:29 +0530

    data transfer useNavigate ke through

--- src/AnkitEcommercePro/About.jsx
+++ src/AnkitEcommercePro/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const About = () => {
+  const location =useLocation()
   return (
     <div>
         <h1>About Page</h1>
+        <h2> User Name is == {location.state.name}</h2>
+        <h3> Email == {location.state.email}</h3>
+        <h3> Password  == {location.state.password}</h3>
+
     </div>
   )
 }
--- src/AnkitEcommercePro/Register.jsx
+++ src/AnkitEcommercePro/Register.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState  } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
   const [name,setName]=useState('')
   const [email,setEmail]=useState('')
   const [password,setPassword]=useState('')
 
+
+   const navigate=useNavigate()
+
+
   const submitbtn =()=>{
-    let items ={name ,email,password}
-    console.log(items)
+   
+    navigate('/about' , {state :{name:name , email:email, password:password}})
   }
+
+
   return (
     <div className='col-sm-4 offset-sm-4'>
       <h1>User Sign Up</h1>
+      <br />
+      <label >User Name</label>
       <input type="text" className='form-control' value={name} onChange={(e)=>setName(e.target.value)} /> <br />
-      <input type="text" className='form-control' value={email} onChange={(e)=>setEmail(e.target.value)} /> <br />
-      <input type="text" className='form-control' value={password} onChange={(e)=>setPassword(e.target.value)} /> <br />
+
+      <label > Enter Email </label>
+      <input type="email" className='form-control' value={email} onChange={(e)=>setEmail(e.target.value)} /> <br />
+      
+      <label >Enter Password</label>
+      <input type="password" className='form-control' value={password} onChange={(e)=>setPassword(e.target.value)} /> <br />
 
       <button className='btn btn-primary' onClick={()=>submitbtn()}>Sign Up</button>
     </div>
